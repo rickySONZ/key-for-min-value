111441cdb55eddcc54391f93fae51085b741ea38
Ryan Erricson <ryanerricson@gmail.com>
2021-02-04 15:15:43 -0500

    commit tree 97bd9160835de3564cf27b4032c74d75971c7905

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    min_key = nil
+    min_val = nil
+    name_hash.each do |name, price|
+        if min_val == nil 
+            min_val = price
+            min_key = name
+        elsif min_val > price
+            min_val = price
+            min_key = name
+        end
+    end
+    min_key
 end
